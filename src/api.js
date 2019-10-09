fbe89ff3d511c275523366a8dd2c58b95481ab9c
Phill <s3607153@student.rmit.edu.au>
2019-10-09 20:56:04 +1100

    added https to api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 function apiGET(action, callback) {
     // URL Of the API server, needs to be changed on deployment
     //fetch('http://localhost:8080/' + action, {
-    fetch('http://popfood-api.ap-southeast-2.elasticbeanstalk.com:80/' + action, {
+    fetch('https://popfood-api.ap-southeast-2.elasticbeanstalk.com:80/' + action, {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
     }).then(response => {
@@ -17,7 +17,7 @@ function apiPOST(action, data, callback) {
     // URL Of the API server, needs to be changed on deployment
     //fetch('http://localhost:8080/' + action, {
     console.log("sending: " + action + " to api server (https://popfood-api.ap-southeast-2.elasticbeanstalk.com:8080")
-    fetch('http://popfood-api.ap-southeast-2.elasticbeanstalk.com:80/' + action, {
+    fetch('https://popfood-api.ap-southeast-2.elasticbeanstalk.com:80/' + action, {
         method: 'POST',
         body: JSON.stringify(data, null, 2), // Prettify the JSON output
         headers: {'Content-Type': 'application/json'}
