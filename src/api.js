25e385e9db90666dbfe33e173ef46b68118923b7
Phill <s3607153@student.rmit.edu.au>
2019-10-10 02:58:30 +1100

    added link to api gateway

--- src/api.js
+++ src/api.js
@@ -16,11 +16,13 @@ function apiGET(action, callback) {
 function apiPOST(action, data, callback) {
     // URL Of the API server, needs to be changed on deployment
     //fetch('http://localhost:8080/' + action, {
+    const apiKey = "7XYW7gkNuN1yLVR7lDBAd2QNos27OQfIaYLllYiC";
+
     console.log("sending: " + action + " to api server")
-    fetch('https://popfood-api.ap-southeast-2.elasticbeanstalk.com:80/' + action, {
+    fetch('https://3zwbp6vz98.execute-api.ap-southeast-2.amazonaws.com/charlie/' + action, {
         method: 'POST',
         body: JSON.stringify(data, null, 2), // Prettify the JSON output
-        headers: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json', 'x-api-key': apiKey}
     }).then(response => {
         return response.json();
     }).then(data => {
