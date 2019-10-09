76348f4bef09bfdda727179c2aca4a5889f2ba85
Phill <s3607153@student.rmit.edu.au>
2019-10-09 18:53:32 +1100

    adding println to console log for api call

--- src/api.js
+++ src/api.js
@@ -16,6 +16,7 @@ function apiGET(action, callback) {
 function apiPOST(action, data, callback) {
     // URL Of the API server, needs to be changed on deployment
     //fetch('http://localhost:8080/' + action, {
+    console.log("sending: " + action + " to api server (https://popfood-api.pdfmrzm3qn.ap-southeast-2.elasticbeanstalk.com:8080")
     fetch('https://popfood-api.pdfmrzm3qn.ap-southeast-2.elasticbeanstalk.com:8080' + action, {
         method: 'POST',
         body: JSON.stringify(data, null, 2), // Prettify the JSON output
