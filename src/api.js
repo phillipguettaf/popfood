2dc3ba4c4d0f05471d83d4ca570f31fb884f9efc
Phill <s3607153@student.rmit.edu.au>
2019-10-09 21:50:23 +1100

    added https to fetch request

--- src/api.js
+++ src/api.js
@@ -17,7 +17,7 @@ function apiPOST(action, data, callback) {
     // URL Of the API server, needs to be changed on deployment
     //fetch('http://localhost:8080/' + action, {
     console.log("sending: " + action + " to api server")
-    fetch('popfoodapi-1937879999.ap-southeast-2.elb.amazonaws.com:443/' + action, {
+    fetch('https://popfoodapi-1937879999.ap-southeast-2.elb.amazonaws.com:443/' + action, {
         method: 'POST',
         body: JSON.stringify(data, null, 2), // Prettify the JSON output
         headers: {'Content-Type': 'application/json'}
