296953fcb8b276a7841edc2b947cd794860ddd57
Phill <s3607153@student.rmit.edu.au>
2019-10-10 03:45:09 +1100

    minor ui changes

--- src/SideDrawer.js
+++ src/SideDrawer.js
@@ -4,6 +4,7 @@ import "./SideDrawer.css"
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
 
 class SideDrawer extends Component {
 
